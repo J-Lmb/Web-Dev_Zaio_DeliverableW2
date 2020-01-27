6d9e5f48659f3cac2e0c406e25121b23fcc53629
Joseph Lumbahe <39498415+J-Lmb@users.noreply.github.com>
2020-01-27 08:59:57 +0200

    Incrementer updated

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ function colourSelect(colour) {
     document.getElementById("colourlabel1").innerHTML = colour;
 
 }
+
+function increaseValue() {
+  var value = parseInt(document.getElementById('number').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value++;
+  document.getElementById('number').value = value;
+}
+
+function decreaseValue() {
+  var value = parseInt(document.getElementById('number').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value < 1 ? value = 1 : '';
+  value--;
+  document.getElementById('number').value = value;
+}
