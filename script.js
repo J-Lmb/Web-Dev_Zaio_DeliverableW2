e6022f016f5af34349ce90febbd05729250be204
Joseph Lumbahe <39498415+J-Lmb@users.noreply.github.com>
2020-01-27 08:38:02 +0200

    Update version

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 /*
 Currently works only on Webkit browsers
 */
+var colorPriceArray = new Array(18)
+
+
 function colourSelect(colour) {
+    
     document.getElementById("colourlabel1").innerHTML = colour;
+
 }
